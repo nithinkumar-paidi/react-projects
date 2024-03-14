57820c3df17b4b64ae792d298d3577d54f0b03fa
npx <nithinpaidi@lyrostech.com>
2024-03-14 20:31:13 +0530

    new updated

---
+++ src/Webpage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import pic1 from './image.jpg'
+
+export default function Webpage() {
+  return (
+    <div>
+         <h2 style={{color:'white', backgroundColor:'blue', textAlign:'center'}}>My Projects</h2>
+         <h3><li><a href=" https://nithinkumar-paidi.github.io/ecomm-react-coffee/">coffee shop</a></li></h3>
+   <h3><li>  <a href=" https://nithinkumar-paidi.github.io/myapp/"> Myapp</a></li></h3>
+   <h3><li> <a href=" https://nithinkumar-paidi.github.io/social-react/"> social-react</a></li></h3>
+  
+
+    </div>
+  )
+}
